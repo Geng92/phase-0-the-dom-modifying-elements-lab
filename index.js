b6597e12d47e929404f66ba67ecb86b075c196ab
Geng-Wung <gengxis92@gmail.com>
2022-09-01 13:43:39 -0400

    commit tree f802c3e67ce36c5710db4d282588b4b061d5a429

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+const maindelete = document.querySelector('main');
+maindelete.remove();
+
+let newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id',"victory");
+
+newHeader.textContent = 'Geng is the champion';
+
+document.append(newHeader);
+
+
+
+//let newHeader = document.getElementById( "victory");
+//document.body.innerHTML = "<h1>YOUR-NAME is the champion</h1>";
